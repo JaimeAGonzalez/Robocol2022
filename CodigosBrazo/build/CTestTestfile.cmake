ee62f706164e1f541f48acc17af3c1c46f8b5039
JaimeAGonzalez <67431910+JaimeAGonzalez@users.noreply.github.com>
2022-10-06 22:58:29 -0500

    Add files via upload

---
+++ CodigosBrazo/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sinfonia/Brazo2022/src
+# Build directory: /home/sinfonia/Brazo2022/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("joy_node")
